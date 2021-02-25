c6674e41d426f5aec94808b839a3e0d0187205c9
ZornitsaAsanska <zori.asanska@gmail.com>
2021-02-25 01:11:54 +0000

    Init

---
+++ coordinates.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from convertbng.util import convert_lonlat
+
+df_coord = pd.read_csv("Model Data - Coordinates.csv")
+eastings = df_coord["Average of Easting"]
+northings = df_coord["Average of Northing"]
+
+list_lon_lat = convert_lonlat(eastings, northings)
+list_lon_lat = np.array(list(zip(list_lon_lat[0], list_lon_lat[1])))
+np.savetxt("coordinates.csv", list_lon_lat, delimiter=',')
