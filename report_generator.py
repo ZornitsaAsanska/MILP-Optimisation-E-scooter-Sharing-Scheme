490e58a530ad4e44d7c56915e9de12ff799455a5
ZornitsaAsanska <zori.asanska@gmail.com>
2021-03-19 20:34:06 +0000

    really messy

---
+++ report_generator.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+class Report():
+
+    def __init__(self, fname, modelName, loc, minimize=True) -> None:
+        self.fname = fname
+        self.modelName = modelName
+        self.loc = loc
+        self.minimize = minimize
+        try:
+            os.mkdir(modelName)
+        except OSError:
+            print(f"Could not create directory {modelName}. It probably already exists")
+
+def station_stats(trips: dict):
+    with pd.ExcelWriter('Station statistics.xlsx') as writer1:
+        for k in trips.keys():
+            trips[k].to_excel(writer1, sheet_name=k, index=False)
+
+            
+
